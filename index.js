9f36a2cef1716747d892e5846bfe15598df7b6db
banurekhaMohan279 <banurekha279@gmail.com>
2021-01-08 21:36:33 +0530

    Fix click header redirect

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 function toggleSection(event){
-  let selectedElt = document.getElementById(event.target.text);
+  let currentTab = event.target.text;
+  if(event.target.text.trim()=='BANUREKHA MOHAN') currentTab = 'About';
+  let selectedElt = document.getElementById(currentTab);
   if(!selectedElt.classList.contains('show')){
     selectedElt.classList.remove('hide');
     selectedElt.classList.add('show');
   }
   let allElts = ['About','Projects','Contact'];
-  let otherElts = allElts.filter(item => item!=event.target.text);
+  let otherElts = allElts.filter(item => item!==currentTab);
   for(let item of otherElts){
     let otherElt = document.getElementById(item);
     if(otherElt.classList.contains('show')){
