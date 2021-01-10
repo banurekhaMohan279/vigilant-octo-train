a3d2b3e3d40d3f7f7c2355248ebfec081aea9d71
banurekhaMohan279 <banurekha279@gmail.com>
2021-01-10 17:59:10 +0530

    Fix css Animation

--- index.js
+++ index.js
@@ -13,16 +13,16 @@ function toggleSection(event){
   if(event.target.text.trim()=='BANUREKHA MOHAN') currentTab = 'About';
   let selectedElt = document.getElementById(currentTab);
   if(!selectedElt.classList.contains('show')){
-    selectedElt.classList.remove('hide', 'transitionEffect');
-    selectedElt.classList.add('show', 'transitionEffect');
+    selectedElt.classList.remove('hide');
+    selectedElt.classList.add('show');
   }
   let allElts = ['About','Projects','Contact'];
   let otherElts = allElts.filter(item => item!==currentTab);
   for(let item of otherElts){
     let otherElt = document.getElementById(item);
     if(otherElt.classList.contains('show')){
-      otherElt.classList.remove('show', 'transitionEffect');
-      otherElt.classList.add('hide', 'transitionEffect');
+      otherElt.classList.remove('show');
+      otherElt.classList.add('hide');
     }
   }
 }
