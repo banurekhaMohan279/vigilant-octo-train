3bedb0cb8c52755a745941b84df5fc2895c4fef3
banurekhaMohan279 <banurekha279@gmail.com>
2021-07-12 14:32:31 +0530

    Add changes to fix flash of unstyled content (FOUC)

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector("#portfolio-container").style.display = "none";
-    document.querySelector("#loader").style.display = "block";
-  });
+/*document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("#portfolio-container").style.display = "none";
+  document.querySelector("#loader").style.display = "block";
+});*/
 
-  window.onload = function() {
-    document.querySelector("#loader").style.display = "none";
-    document.querySelector("#portfolio-container").style.display = "block";
-  }
+window.onload = function() {
+  document.querySelector("#loader").style.display = "none";
+  document.querySelector("#portfolio-container").style.display = "block";
+};
 
-function toggleSection(event){
+function toggleSection(event) {
   let currentTab = event.target.text;
-  if(event.target.text.trim()=='BANUREKHA MOHAN') currentTab = 'About';
+  if (event.target.text.trim() == "BANUREKHA MOHAN") currentTab = "About";
   let selectedElt = document.getElementById(currentTab);
-  if(!selectedElt.classList.contains('show')){
-    selectedElt.classList.remove('hide');
-    selectedElt.classList.add('show');
+  if (!selectedElt.classList.contains("show")) {
+    selectedElt.classList.remove("hide");
+    selectedElt.classList.add("show");
   }
-  let allElts = ['About','Contact'];
-  let otherElts = allElts.filter(item => item!==currentTab);
-  for(let item of otherElts){
+  let allElts = ["About", "Projects", "Contact"];
+  let otherElts = allElts.filter(item => item !== currentTab);
+  for (let item of otherElts) {
     let otherElt = document.getElementById(item);
-    if(otherElt.classList.contains('show')){
-      otherElt.classList.remove('show');
-      otherElt.classList.add('hide');
+    if (otherElt.classList.contains("show")) {
+      otherElt.classList.remove("show");
+      otherElt.classList.add("hide");
     }
   }
 }
